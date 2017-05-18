65e79f36c29bd4634ec00662b63268ac1d32d870
Abhilash Juluri <ajuluri@gmail.com>
2017-05-18 00:19:13 -0400

    added business methods

--- tssync/src/main/java/org/wpb/integration/tssync/TargetSolutionsRestClient.java
+++ tssync/src/main/java/org/wpb/integration/tssync/TargetSolutionsRestClient.java
@@ -12,7 +12,9 @@ import javax.ws.rs.core.Response;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.wpb.integration.tssync.utils.PropertiesUtils;
+import org.wpb.targetsolutions.entities.Credentials;
 import org.wpb.targetsolutions.entities.Employee;
+import org.wpb.targetsolutions.entities.Groups;
 import org.wpb.targetsolutions.entities.Users;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -31,7 +33,7 @@ public class TargetSolutionsRestClient {
 	}
 
 	/**
-	 * Given a Target Solutions userID, this method returns Employee POJO including Links to related entities
+	 * Given a Target Solutions userID, this method returns Employee including Links to related entities
 	 * @param userID
 	 * @return {@link Employee}
 	 * @throws IOException
@@ -55,6 +57,12 @@ public class TargetSolutionsRestClient {
 		return employee;
 	}
 
+	/**
+	 * Given a WPB Employee ID, this method returns Employee including Links to related entities
+	 * @param empNo
+	 * @return {@link Employee}
+	 * @throws IOException
+	 */
 	public Employee getEmployeeByEmpNo(String empNo) throws IOException {
 		Users employee = new Users();
 
@@ -74,6 +82,35 @@ public class TargetSolutionsRestClient {
 		return employee.getUsers().get(0);
 	}
 
+	public Credentials getEmployeeCredentials(String empNo) throws IOException {
+		return new Credentials();
+		
+	}
+	
+	public Groups getEmployeeGroups(String empNo) throws IOException {
+		return new Groups();
+	}
+	
+	public void updateEmployee(Employee emp) {
+		//TODO
+	}
+	
+	public void updateEmployeeCredentials(String empNo, List<Credentials> credentials) {
+		//TODO
+	}
+
+	public void updateEmployeeGroups(String empNo, List<Groups> groups) {
+		//TODO
+	}
+	
+	public void createEmployee(Employee emp, List<Credentials> credentials, List<Groups> groups) {
+		//TODO
+	}
+	
+	public void deleteEmployee(String empNo) {
+		//TODO
+	}
+	
 	public WebTarget getMainSite() throws IOException {
 		return ClientBuilder.newClient().target(PropertiesUtils.getSiteURI());
 	}
@@ -90,7 +127,14 @@ public class TargetSolutionsRestClient {
 		return getMainSite().path("credentials");
 	}
 
-	public WebTarget getCredentials(String userid, String credentialid) throws IOException {
+	/**
+	 * This method returns Credentials URL using credentialid directly
+	 * @param userid
+	 * @param credentialid
+	 * @return {@link WebTarget}
+	 * @throws IOException
+	 */
+	public WebTarget getCredentials(String credentialid) throws IOException {
 		return getCredentialsSite().path(credentialid);
 	}
 
